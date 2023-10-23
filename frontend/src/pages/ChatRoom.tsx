23be37bbc3f24d8ec161e7d6090d6d4fb9578ddf
Srikar KOUSHIK <srikar.koushik@amadeus.com>
2023-10-24 01:05:00 +0530

    secure wss

--- frontend/src/pages/ChatRoom.tsx
+++ frontend/src/pages/ChatRoom.tsx
@@ -45,7 +45,7 @@ export default function ChatRoom() {
       toast.success(state.message);
     }
 
-    ws.current = new WebSocket('ws://dorms-chat.onrender.com:8080');
+    ws.current = new WebSocket('wss://dorms-chat.onrender.com:8080');
 
     ws.current.addEventListener('open', () => {
       ws.current?.send(
